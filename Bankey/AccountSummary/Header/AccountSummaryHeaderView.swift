77d69ea5343bb5d4832ee85fdeca1d49551cfee1
Faust <mrtracker77@gmail.com>
2022-11-15 23:14:01 +0300

    feat: Add networking to AccountSummary

--- Bankey/AccountSummary/Header/AccountSummaryHeaderView.swift
+++ Bankey/AccountSummary/Header/AccountSummaryHeaderView.swift
@@ -11,7 +11,22 @@ import UIKit
 class AccountSummaryHeaderView: UIView {
     
     @IBOutlet var contentView: UIView!
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
     let shakeyBellView = ShakeyBellView()
+    
+    struct ViewModel {
+        let welcomeMessage: String
+        let name: String
+        let date: Date
+        
+        var dataFormatted: String {
+            return date.monthDayYearString
+        }
+    }
     
     override init(frame: CGRect) {
             super.init(frame: frame)
@@ -52,4 +67,9 @@ class AccountSummaryHeaderView: UIView {
         ])
     }
 
+    func configure(viewModel: ViewModel) {
+        welcomeLabel.text = viewModel.welcomeMessage
+        nameLabel.text = viewModel.name
+        dateLabel.text = viewModel.dataFormatted
+    }
 }
